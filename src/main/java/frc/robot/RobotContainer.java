f3bd499533f363f5decc008696448b215fc1f701
apanteli23 <54606854+apanteli23@users.noreply.github.com>
2021-02-05 18:51:49 -0500

    Updated Autonomous

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import frc.robot.subsystems.DriveBase.DriveSubsystem;
 import frc.robot.subsystems.DriveBase.Drive;
 import frc.robot.Constants.IOConstants;
 import frc.robot.control.*;
+import frc.robot.Autonomous.BasicAutonomous;
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -24,14 +25,14 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final DriveSubsystem m_drive = new DriveSubsystem();
  
-  public static XboxController m_driverController = new XboxController(IOConstants.kDriverControllerPort);
+  public static XboxController m_controller = new XboxController(IOConstants.kDriverControllerPort);
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
       
-    new Drive(m_drive, () -> m_driverController.getRightY(), () -> m_driverController.getLeftY());
+    new Drive(m_drive, () -> m_controller.getRightY(), () -> m_controller.getLeftY());
 
     // Configure the button bindings
     configureButtonBindings();
@@ -44,6 +45,8 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
+    new XboxControllerButton(m_controller, XboxController.Button.kA)
+      .whenPressed(new BasicAutonomous(m_drive));
   }
 
 
--- src/main/java/frc/robot/control/XboxControllerButton.java
+++ src/main/java/frc/robot/control/XboxControllerButton.java
@@ -1,13 +1,5 @@
 package frc.robot.control;
 
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2008-2017 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Button;
 
 /**
